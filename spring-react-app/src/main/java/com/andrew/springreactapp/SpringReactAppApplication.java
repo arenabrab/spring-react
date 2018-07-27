cb2e0427be87b819e57bec88535c5cf48e483dad
Andrew Barbanera <andrewbarbanera@gmail.com>
2018-07-27 14:11:40 -0400

    Initial commit - adding spring-react-app template

---
+++ spring-react-app/src/main/java/com/andrew/springreactapp/SpringReactAppApplication.java
@@ -0,0 +1,12 @@
+package com.andrew.springreactapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactAppApplication.class, args);
+	}
+}
